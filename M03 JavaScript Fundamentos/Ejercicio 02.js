846d56648981fddd18ff386e60dd4450b64f2be8
Juan <larrota.juancamilo@gmail.com>
2023-03-17 17:46:28 -0500

    Modulo 03 de Prop Course-SoyHenry

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,36 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   let suma = x+y;
+   return suma;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   let resta = x-y;
+   return resta;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   let division = x/y;
+   return division;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   let multiplication = x*y;
+   return multiplication;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   let resto = x%y;
+   return resto;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
