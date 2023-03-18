4019cb830f330047e98a1b2c37a1bae9a675c897
Juan <larrota.juancamilo@gmail.com>
2023-03-18 13:36:16 -0500

    Modulo 08 de Prep Course SoyHenrry

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,12 +5,16 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   let temp = nombre.split("");
+   temp[0] = temp[0].toUpperCase();
+   return temp.join('');
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -18,6 +22,7 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb(num1,num2);
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -26,6 +31,11 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   let total = 0;
+   arrayOfNumbers.forEach(num =>{
+      total +=num;
+   });
+   cb(total);
 }
 
 function forEach(array, cb) {
@@ -33,6 +43,9 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   array.forEach( element =>{
+      cb(element);
+   });
 }
 
 function map(array, cb) {
@@ -40,12 +53,25 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   let newArray = array.map(element =>{
+      return cb(element);
+   });
+   return newArray;
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+
+   function startA(element){
+      if(element[0] != "a"){
+         return false
+      }
+      return element
+   }
+   let aArrray = arrayOfStrings.filter(startA);
+   return aArrray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
